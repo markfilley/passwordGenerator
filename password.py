f37dca5584f589c8f3f5ef3e646cd8924003aaaf
mark.filley <mark.filley@gmail.com>
2022-01-24 07:33:35 -0600

    improve special characters

--- password.py
+++ password.py
@@ -16,6 +16,7 @@ def passwordGenerator(args):
     passPhrase = ''
     capitalize = False
     useNumbers = False
+    specialEnd = True
     numNumbers = 0
     numSpeicals = 0
     countNumbers = 0
@@ -25,10 +26,13 @@ def passwordGenerator(args):
 
     length = 0
 
-    specialList1 = '!@#$%^&*.-_'
-    specialList2 = ',<>?/[]:;{}'
-    specialList3 = '!#.-^_'
+    specialSet = 1
+    specialList1 = '!@#$%^&*.-_' # special set of ASCII characters
+    specialList2 = ',<>?/[]:;{}' # extended set of speical ASCII characters
+    specialList3 = '!#.-^_' # limited, some systems only allow basic special characters
+    specialList4 = '!@#$%^&*.-_,<>?/[]:;{}'  # full list
 
+    specialList = ''
 
     if (args.length is None):
         length = 15
@@ -36,6 +40,7 @@ def passwordGenerator(args):
         length = args.length
         if (args.length < 1):
             print ("numWords needs to be a positive integer")
+            exit()
 
     if (args.numNumber is None):
         numNumbers = 3
@@ -43,6 +48,29 @@ def passwordGenerator(args):
         numNumbers = args.numNumber
         if (args.numNumber < 1):
             print ("ERROR: numNumbers needs to be a positive integer")
+            exit()
+
+    if (args.specialSet is None):
+        specialSet = 1
+        specialList = specialList1
+        print ("Here0")
+    elif (isinstance(args.specialSet, int)):
+        specialSet = args.specialSet
+        if (specialSet == 1):
+            specialList = specialList1
+            print ("Here1")
+        elif (specialSet == 2):
+            specialList = specialList2
+            print ("Here2")
+        elif (specialSet == 3):
+            specialList = specialList3
+            print ("Here3")
+        elif (specialSet == 4):
+            specialList = specialList4
+            print ("Here4")
+        else:
+            print ("ERROR: special set must be a number.")
+            print ("Here5")
             exit()
 
     if (args.numSpecial is None):
@@ -62,6 +90,11 @@ def passwordGenerator(args):
         capitilize = False
     elif (args.capitalize.lower().strip("'") == "true" or args.capitalize.lower().strip("'") == "t"):
         capitalize = True
+
+    if (args.specialEnd is None):
+        specialEnd = True
+    elif (args.specialEnd.lower().strip("'") == "false" or args.specialEnd.lower().strip("'") == "f"):
+        specialEnd = False
 
     if (args.ambiguous is None):
         ambiguous = False
@@ -89,19 +122,28 @@ def passwordGenerator(args):
 
     for i in range(quantity):
         countNumbers = 0
-        countSpecial = 0
+        countSpecials = 0
         password = ''
         for j in range(length):
 
             characterType = random.randrange(1,4)
             if (characterType == 1):
-                password = password + random.choice(abcList)
+                if (capitalize and (random.randrange(1,2)==1)):
+                    password = password + random.choice(abcList).upper()
+                else:
+                    password = password + random.choice(abcList)
             elif ((characterType == 2) and (countNumbers < numNumbers)):
                 password = password + random.choice(numberList)
                 countNumbers = countNumbers + 1
             elif ((characterType == 3) and (countSpecials < numSpecials)):
-                password = password + random.choice(specialList1)
-                countSpecials = countSpecials + 1
+                if ((j == 0) or (j == length -1)) and specialEnd:
+                    countSpecials = countSpecials + 1
+                    password = password + random.choice(specialList)
+                elif (j > 0 and j < length -1):
+                    password = password + random.choice(specialList)
+                    countSpecials = countSpecials + 1
+                else:
+                    password = password + random.choice(abcList)
             else:
                 password = password + random.choice(abcList)
 
@@ -120,5 +162,7 @@ if __name__ == '__main__':
     parser.add_argument('--numNumber', type=int, help='number of numbers, default is 2')
     parser.add_argument('--quantity', type=int, help='how many passwords to generate, default is 1')
     parser.add_argument('--ambiguous', type=ascii, help='do not use characters that look similar, like 1 and l')
+    parser.add_argument('--specialEnd', type=ascii, help='allow special characters at beginning and end of password, default True')
+    parser.add_argument('--specialSet', type=int, help="special set 1 = '!@#$%^&*.-_'  ', 2 = <>?/[]:;{}' 3 = '!#.-^_' 4 = '!@#$%^&*.-_,<>?/[]:;{}' ")
     args = parser.parse_args()
     passwordGenerator(args)
